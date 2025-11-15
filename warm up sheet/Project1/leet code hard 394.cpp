f7b4f0c753beb29666de03daab65489ef8b9b2ba
omar <mmaro5584@gmail.com>
2025-11-15 15:01:00 +0200

    feat:adding recursion method to hard question

--- warm up sheet/Project1/leet code hard 394.cpp
+++ warm up sheet/Project1/leet code hard 394.cpp
@@ -1,3 +1,5 @@
+
+/// by loop
 //#include<iostream>
 //#include<string>
 //#include<iterator>
@@ -63,3 +65,80 @@
 //	cout << decodeString(s);
 //
 //}
+
+
+/// by recursion
+
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+
+int numOfRepeating(string& s,size_t indexOfBracet,short& cntOfDigits )
+{
+	int res = 0;
+	while (isdigit(s[indexOfBracet]))
+	{
+		res += (s[indexOfBracet]-'0') * pow(10, cntOfDigits);
+		cntOfDigits++;
+		if (indexOfBracet==0)
+		{
+			break;
+		}
+			indexOfBracet--;
+	}
+	return res;
+
+}
+string decodeString(string& s) {
+
+
+	if (s.find_last_of('[') == string::npos)
+	{
+		return s;
+	}
+	string result="";
+	size_t start,end;
+
+
+	short cntOfDigits = 0;
+		int repeating = 0;
+		string str_2_replace="";
+
+		start= s.find_last_of('[');
+		
+		end=  s.find_first_of(']',start);
+		
+		string str_2_repeat=s.substr(start+1,end-start-1);
+		repeating = numOfRepeating(s, start - 1,cntOfDigits);
+		
+		
+		while (repeating--)
+		{
+			str_2_replace += str_2_repeat;
+		}
+		
+		s.replace(start-cntOfDigits,end-start+cntOfDigits+1,str_2_replace);
+		
+		str_2_repeat.clear();
+		
+
+
+	
+		if (s.find_last_of('[') != string::npos)
+	{
+		 decodeString(s);
+	}
+		return s;
+}
+
+
+
+int main()
+{
+	string s;
+	cin >> s;
+	cout << decodeString(s);
+
+}
