4cdb31f7eda8ec188f471491a934df63cef553a4
omar <mmaro5584@gmail.com>
2026-02-03 18:41:06 +0200

    solved problem-2 medium

--- arraylist sheet/arraylist sheet/arraylist sheet/problem_2.cpp
+++ arraylist sheet/arraylist sheet/arraylist sheet/problem_2.cpp
@@ -1,5 +1,32 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
+
+
+
+class Solution {
+public:
+
+	int findMaxConsecutiveOnes(vector<int>& nums) {
+
+		auto start = nums.begin();
+		auto end = nums.end();
+		int maxlen = 0;
+
+		do {
+			end = find(start, nums.end(), 0);
+			maxlen = max(int(end - start), maxlen);
+			if (end != nums.end())
+			{
+				start = end + 1;
+			}
+
+
+		} while (end < nums.end());
+		return maxlen;
+
+	}
+};
 
 
