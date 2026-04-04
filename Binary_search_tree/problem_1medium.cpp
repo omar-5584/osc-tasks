44b00df3b1b5771293ff2076c9a397d01a792e47
omar ibrahim <mmaro5584@gmail.com>
2026-04-04 15:28:06 +0200

    solved problem 1 medium in sheet

---
+++ Binary_search_tree/problem_1medium.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+
+          if(root->val<q->val && root->val<p->val)
+         {
+            return lowestCommonAncestor(root->right,p,q);
+         }
+        else if(root->val>q->val && root->val>p->val)
+         {
+             return lowestCommonAncestor(root->left,p,q);
+         }
+          return root;
+   }
+};
