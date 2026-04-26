7928f6f98c2b39169f30066dcbfb47ba2fbedb88
omar ibrahim <mmaro5584@gmail.com>
2026-04-27 00:34:32 +0300

    solve problem 3 medium bst

---
+++ Binary_search_tree/problem3_medium.cpp
@@ -0,0 +1,37 @@
+
+ #include <vector>
+using namespace std;
+
+
+
+
+class Solution {
+public:
+    TreeNode* sortedArrayToBST(vector<int>& nums) {
+       
+        return build(nums, 0, nums.size() - 1);
+    }
+
+private:
+    TreeNode* build(vector<int>& nums, int left, int right) {
+    
+        if (left > right) {
+            return nullptr;
+        }
+
+        
+        int mid = left + (right - left) / 2;
+
+        
+        TreeNode* root = new TreeNode(nums[mid]);
+
+
+        root->left = build(nums, left, mid - 1);
+        root->right = build(nums, mid + 1, right);
+
+        
+        return root;
+    }
+
+
+};
